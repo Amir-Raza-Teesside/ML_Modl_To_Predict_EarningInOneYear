abaebc3161d8ebd89d1b59946416b8797e265712
Fattani Computers <amirraza2501@gmail.com>
2022-06-29 17:39:49 +0100

    Machine Learning Model

---
+++ app/src/main/java/uk/ac/tees/aad/ml_model_to_predict_minimunearningthrashold/MainActivity.java
@@ -0,0 +1,16 @@
+package uk.ac.tees.aad.ml_model_to_predict_minimunearningthrashold;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"as",Toast.LENGTH_LONG).show();
+    }
+}
