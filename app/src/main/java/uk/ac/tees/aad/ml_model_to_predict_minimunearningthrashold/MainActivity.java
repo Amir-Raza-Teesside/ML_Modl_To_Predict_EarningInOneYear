3799b3e3fa2c95062a274ca8c3b0f14e586976d3
Fattani Computers <amirraza2501@gmail.com>
2022-06-30 03:15:17 +0100

    Designing

--- app/src/main/java/uk/ac/tees/aad/ml_model_to_predict_minimunearningthrashold/MainActivity.java
+++ app/src/main/java/uk/ac/tees/aad/ml_model_to_predict_minimunearningthrashold/MainActivity.java
@@ -3,15 +3,112 @@ package uk.ac.tees.aad.ml_model_to_predict_minimunearningthrashold;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner Gender;
+    Spinner occupation;
+    Spinner Race;
+    Spinner Workplace;
+    Button button;
+    static String SelectedWorkplace="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this,"as",Toast.LENGTH_LONG).show();
-        Toast.makeText(MainActivity.this,"abs",Toast.LENGTH_LONG).show();
+
+        Gender = findViewById(R.id.genderSpiner);
+        occupation = findViewById(R.id.OccupationSpiner);
+        Workplace = findViewById(R.id.workplaceSpiner);
+        Race = findViewById(R.id.racespinner);
+        button = findViewById(R.id.button);
+
+
+       ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+               R.array.Gender, android.R.layout.simple_spinner_item);
+       adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       Gender.setAdapter(adapter);
+       Gender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+           @Override
+           public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+           }
+
+           @Override
+           public void onNothingSelected(AdapterView<?> parent) {
+
+           }
+       });
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.Occupation, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        occupation.setAdapter(adapter2);
+        occupation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.race, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Race.setAdapter(adapter3);
+        Race.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
+                R.array.workplace, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Workplace.setAdapter(adapter4);
+        Workplace.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(MainActivity.this,"Workplace",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
+
+
 }
